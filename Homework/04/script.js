908b05750e1725c67006611c1ed8c61f2438a6b1
Stefan Velev <sdvelev@gmail.com>
2023-06-06 20:02:53 +0300

    Add final version of homework 4

--- Homework/04/script.js
+++ Homework/04/script.js
@@ -11,13 +11,8 @@ formElement.addEventListener('submit', function (event) {
 
     event.preventDefault();
 
-    const errorResponses = document.querySelectorAll('.error');
-    errorResponses.forEach((errorResponse) => {
-        errorResponse.remove();
-    });
-
-    const successResponse = document.getElementById('success');
-    successResponse.style.display = 'none';
+    removeErrorMessages();
+    hideSuccessMessage();
 
     const userNameValue = userNameInputElement.value;
     const nameValue = nameInputElement.value;
@@ -27,34 +22,33 @@ formElement.addEventListener('submit', function (event) {
     const postalCodeValue = postalCodeInputElement.value;
 
     let isAvailableError = false;
-
     if (isWrongFormatUserName(userNameValue)) {
-        visualizeError(userNameInputElement, 'Невалидно потребителско име. Трябва да е между 3 и 10 симовла.');
+        visualizeError(userNameInputElement, 'Невалидно потребителско име! Трябва да е между 3 и 10 симовла.');
         isAvailableError = true;
     }
 
     if (isWrongFormatName(nameValue)) {
-        visualizeError(nameInputElement, 'Невалидно име. Трябва да бъде максимум 50 символа.');
+        visualizeError(nameInputElement, 'Невалидно име! Трябва да бъде максимум 50 символа.');
         isAvailableError = true;
     }
 
     if (isWrongFormatName(familyNameValue)) {
-        visualizeError(familyNameInputElement, 'Невалидна фамилия. Трябва да бъде максимум 50 символа.');
+        visualizeError(familyNameInputElement, 'Невалидна фамилия! Трябва да бъде максимум 50 символа.');
         isAvailableError = true;
     }
 
     if (isWrongFormatEmail(emailValue)) {
-        visualizeError(emailInputElement, 'Невалиден имейл. Трябва да въведете валиден имейл формат.');
+        visualizeError(emailInputElement, 'Невалиден имейл! Трябва да въведете валиден имейл формат.');
         isAvailableError = true;
     }
 
     if (isWrongFormatPassword(passwordValue)) {
-        visualizeError(passwordInputElement, 'Невалидна парола. Паролата трябва да бъде между 6 и 10 символа и трябва да включва поне една главна буква, малка буква и цифра.');
+        visualizeError(passwordInputElement, 'Невалидна парола! Паролата трябва да бъде между 6 и 10 символа и трябва да включва поне една главна буква, малка буква и цифра.');
         isAvailableError = true;
     }
 
     if (postalCodeValue !== "" && isWrongFormatPostalCode(postalCodeValue)) {
-        visualizeError(postalCodeInputElement, 'Невалиден пощенски код. Трябва да бъде във формат 11111-1111 (само цифри) или формат 1111(само цифри).');
+        visualizeError(postalCodeInputElement, 'Невалиден пощенски код! Трябва да бъде във формат 11111-1111 (само цифри) или формат 1111(само цифри).');
         isAvailableError = true;
     }
 
@@ -65,12 +59,24 @@ formElement.addEventListener('submit', function (event) {
     doesUserAlreadyExist(userNameValue)
         .then(userNameFound => {
             if (userNameFound) {
-                visualizeError(userNameInputElement, 'Нввалидно потребителско име. Вече съществува потребител с такова потребителско име.');
+                visualizeError(userNameInputElement, 'Невалидно потребителско име! Вече съществува потребител с такова потребителско име.');
             } else {
                 document.getElementById('success').style.display = 'block';
             }
         })
 });
+
+function removeErrorMessages() {
+    const errorResponses = document.querySelectorAll('.error');
+    errorResponses.forEach((errorResponse) => {
+        errorResponse.remove();
+    });
+}
+
+function hideSuccessMessage() {
+    const successResponse = document.getElementById('success');
+    successResponse.style.display = 'none';
+}
 
 function visualizeError(inputElementToDisplayAfter, errorMessage) {
     const errorDiv = document.createElement('div');
@@ -154,6 +160,7 @@ function doesUserAlreadyExist(username) {
             return usernamesCollection.includes(username);
         })
         .catch(error => {
+            console.error(error);
             return false;
         });
 }
